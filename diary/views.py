010e673ae3b7beb380ba009f99d614345d30285c
isaisa01199898 <isamikonoue0119@gmail.com>
2025-03-15 15:56:30 +0900

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -9,9 +9,7 @@ class IndexView(View):
             ZoneInfo("Asia/Tokyo")
         ).strftime("%Y年%m月%d日 %H:%M:%S")
         return render(
-            request, "diary/index.htm", {"now_time": now_time})  # 修正: コンテキストを辞書に変更
-    def self_get(self, request, *args, **kwargs):
-        return render(request, "app/index.html")
-
+            request, "diary/index.html", {"now_time": now_time})  # 修正: コンテキストを辞書に変更
+ 
 
 index = IndexView.as_view()
