735d42eb700b742258f86d77307c683f39048998
isaisa01199898 <isamikonoue0119@gmail.com>
2025-03-18 16:00:40 +0900

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -3,14 +3,27 @@ from django.views import View
 from datetime import datetime
 from zoneinfo import ZoneInfo
 ser_data=78
-data=78,68.75.85,92,120
+data1=78
+data2=78
+data3=78
+data4=78
+data5=78
+data6=88
+data7=68
+
 class IndexView(View):
     def get(self, request):
         now_time = datetime.now(
             ZoneInfo("Asia/Tokyo")
         ).strftime("%Y年%m月%d日 %H:%M:%S")
         return render(
-            request, "diary/index.html", {"now_time": now_time , "ser_data":ser_data , "data":data})  # 修正: コンテキストを辞書に変更
+            request, "diary/index.html", {"now_time": now_time , "ser_data":ser_data , "data1":data1 ,data2=78
+data3=78
+data4=78
+data5=78
+data6=88
+data7=68
+})  # 修正: コンテキストを辞書に変更
  
 
 index = IndexView.as_view()
